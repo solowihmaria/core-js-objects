660f4997d1b78909e3d0ed16f39db7ec1f562184
solowihmaria <solowihmaria@gmail.com>
2025-02-25 15:53:19 +0300

    feat: add solution of 8th task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -165,8 +165,29 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let count25 = 0; // Количество купюр 25
+  let count50 = 0; // Количество купюр 50
+
+  return queue.every((bill) => {
+    if (bill === 25) {
+      count25 += 1;
+    } else if (bill === 50) {
+      if (count25 === 0) return false;
+      count25 -= 1;
+      count25 += 1;
+    } else if (bill === 100) {
+      if (count50 > 0 && count25 > 0) {
+        count50 -= 1;
+        count25 -= 1;
+      } else if (count25 >= 3) {
+        count25 -= 3;
+      } else {
+        return false;
+      }
+    }
+    return true;
+  });
 }
 
 /**
