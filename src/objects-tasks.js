827374b0c5686e286285e3bb917044b25a927df5
solowihmaria <solowihmaria@gmail.com>
2025-02-25 16:00:32 +0300

    feat: add solution of 11th task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -237,8 +237,9 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const obj = JSON.parse(json); // преобразуем JSON в объект
+  return Object.assign(Object.create(proto), obj); // поздаём объект с нужным прототипом и копируем свойства
 }
 
 /**
